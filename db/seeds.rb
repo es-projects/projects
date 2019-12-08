c32022ac3d7ad9f3a3f6f294d586a5e2e4f4783e
Diogo Ribeiro <37108144+keiriko@users.noreply.github.com>
2019-12-08 15:38:48 +0000

    Basic actions + seed
    - Basic actions created with "scaffold"
    - Created small seed and tested actions with it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+students = Student.create([{ number: 20165, name:'David Moreno', ects:150, average:12.96, group_id: 0 , project_id: 0 },
+                           { number: 20167, name:'Diogo Ribeiro', ects:150, average:13.84, group_id: 0 , project_id: 0 }])

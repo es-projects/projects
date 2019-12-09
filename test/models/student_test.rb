92ad783f6da40ce0056c6e2dc525985f26039fca
Diogo Ribeiro <37108144+keiriko@users.noreply.github.com>
2019-12-09 22:38:56 +0000

    + test and name valid
    + name can't be null and test

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -4,4 +4,8 @@ class StudentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save student without name" do
+      student = Student.new
+      assert_not student.save, "Saved student without name"
+  end
 end

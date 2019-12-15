5fe889b9d1588e47ca323de80a396cae928955c0
Hélder Lopes <aluno19784@ipt.pt>
2019-12-15 18:44:08 +0000

    Fix codacy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-students = Student.create([{ number: 20165, name:'David Moreno', ects:150, average:12.96, group_id: 0 , project_id: 0 },
+Student.create([{ number: 20165, name:'David Moreno', ects:150, average:12.96, group_id: 0 , project_id: 0 },
                            { number: 20167, name:'Diogo Ribeiro', ects:150, average:13.84, group_id: 0 , project_id: 0 }])
 
 15.times do
@@ -17,7 +17,7 @@ students = Student.create([{ number: 20165, name:'David Moreno', ects:150, avera
     )
 end
 
-applications = ProjectApplication.create([
+ProjectApplication.create([
 	{ group_id: '1', project_id: '1', accepted: 'false' },
 	{ group_id: '1', project_id: '2', accepted: 'true' },
 	{ group_id: '1', project_id: '3', accepted: 'false' },

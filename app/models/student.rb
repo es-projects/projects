f42c84609b80edb559881e5fa921d1b377cf1962
Diogo Ribeiro <37108144+keiriko@users.noreply.github.com>
2019-12-16 00:36:35 +0000

    model validations + tests
    + model fields validation
    + model tests
    + created test cases in students.yml

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student < ApplicationRecord
     validates :name, presence:true
+    validates :average, :ects, :number, :group_id, :project_id, presence: true
+	validates :number, :group_id, :project_id, numericality: {only_integer: true, :greater_than => 0}
+    validates :ects, numericality: {only_integer: true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 180}
+    validates :average, numericality: {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 20}
+
 end

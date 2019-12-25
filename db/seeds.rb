44ee8d9cd17ba52a185b1d7fb856f981c21cb444
João Canoso <jpcanoso@hotmail.com>
2019-12-25 01:25:31 +0000

    Codacy fix -.-

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-30.times do |n|
+30.times do
     User.create(
         username: Faker::Internet.user_name,
         email: Faker::Internet.email,

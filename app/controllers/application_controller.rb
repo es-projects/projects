cc9e6bf29f31ecd372ac2af29b2697e5be836261
Beril-1 <35508877+Beril-1@users.noreply.github.com>
2020-01-13 23:07:06 +0000

    Adding Translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+around_action :switch_locale
+ 
+def switch_locale(&action)
+  locale = params[:locale] || I18n.default_locale
+  I18n.with_locale(locale, &action)
+end
+
+
     protected
 
     def configure_permitted_parameters

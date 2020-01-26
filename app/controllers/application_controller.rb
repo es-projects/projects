a8c23f4681857e6a463a728016bc916bf6b235b7
João Canoso <jpcanoso@hotmail.com>
2020-01-26 16:56:42 +0000

    Added users translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-around_action :switch_locale
- 
-def switch_locale(&action)
-  locale = params[:locale] || I18n.default_locale
-  I18n.with_locale(locale, &action)
-end
+    def default_url_options
+    { locale: I18n.locale }
+    end
+    
+    around_action :switch_locale
+    
+    def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+    end
 
 
     protected

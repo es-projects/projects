607993ca05c1890b7ad619c36d2f83dd58e57874
João Canoso <jpcanoso@hotmail.com>
2020-01-27 07:27:35 +0000

    Added applications to projects views
    Updated footer
    Updated navbar with translations
    Added some translations
    Tested and working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @project = Project.find(params[:id])
+    @applications = @project.project_applications
   end
 
   # GET /projects/new
